9ac678c1383825e59323166576115a5fdf05d7fa
perfe-er <zhu063296@gmail.com>
2019-09-07 20:07:36 +0800

    1.添加next按钮,删除question_text文本框的Android：text 2.将toast消息用setGravity方法放置在布局正上方 3.新增Question类，类中封装问题文本及问题答案，生成getter方法和setter方法 4.添加TextView，nextButton变量及Question对象数组，对按钮进行监听：递增数组索引并相应地更新TextView的文本内容 5.引用TextView，并将其文本内容设置为当前数组索引所指向的地理知识问题

--- app/src/main/java/com/example/zwq/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/zwq/geoquiz/MainActivity.java
@@ -3,31 +3,61 @@ package com.example.zwq.geoquiz;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFlaseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private Question[] mQuestionBank=new Question[]{
+            new Question(R.string.question_australia,true),
+            new Question(R.string.question_africa,true),
+            new Question(R.string.question_americas,true),
+            new Question(R.string.question_asia,true),
+            new Question(R.string.question_mideast,true),
+            new Question(R.string.question_oceans,true)
+    };
+    private int mCurrentIndex=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTrueButton = (Button)findViewById(R.id.b1);
+        mQuestionTextView = (TextView)findViewById(R.id.question_text) ;
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+
+        mTrueButton = (Button)findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                Toast toast=Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,10);
+                toast.show();
             }
         });
 
-        mFlaseButton = (Button)findViewById(R.id.b2);
+        mFlaseButton = (Button)findViewById(R.id.false_button);
         mFlaseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,R.string.incorrect_toact,Toast.LENGTH_SHORT).show();
+                Toast toast=Toast.makeText(MainActivity.this,R.string.incorrect_toact,Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,10);
+                toast.show();
+            }
+        });
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex+1)%mQuestionBank.length;
+                int question = mQuestionBank[mCurrentIndex].getTextResId();
+                mQuestionTextView.setText(question);
             }
         });
     }
