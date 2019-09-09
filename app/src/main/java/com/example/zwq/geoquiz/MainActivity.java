097b9e950b894198af26fe4d65ecc33e48bf104b
perfe-er <zhu063296@gmail.com>
2019-09-09 10:58:54 +0800

    1.GeoQuiz应用添加第二个activity，并且添加了CheatActivity(land) 2.在layout/activity_main和layout-land/activity_main中添加cheat_button按钮并对其设置监听 3.用Intent实现两个activity之间的跳转并用使用intent extra实现两个activity间的数据传递 4.在CheatActivity中添加了cheat_button按钮并设置监听

--- app/src/main/java/com/example/zwq/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/zwq/geoquiz/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.zwq.geoquiz;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,13 +15,16 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG="MainActivity";
     private static final String KEY_INDEX="index";
+    private static final int REQUEST_CODE_CHEAT=0;
     private int correctAnswer=0;
     private int answerLength=0;
+    private boolean mIsCheat;
 
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
     private Button mPrevButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
     private Question[] mQuestionBank=new Question[]{
             new Question(R.string.question_australia,true,0),
@@ -44,6 +49,16 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
+
+        mCheatButton=(Button)findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean answerTrue=mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent intent = CheatActivity.newIntent(MainActivity.this,answerTrue);
+                startActivityForResult(intent,REQUEST_CODE_CHEAT);
+            }
+        });
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
@@ -73,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mIsCheat=false;
                 updateQuestion();
                 answerLength++;
                 if(answerLength==mQuestionBank.length){
@@ -87,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mIsCheat=false;
                 mCurrentIndex = (mCurrentIndex-1)%mQuestionBank.length;
                 int question = mQuestionBank[mCurrentIndex].getTextResId();
                 mQuestionTextView.setText(question);
@@ -102,6 +119,18 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent date){
+        if(resultCode!=Activity.RESULT_OK){return;}
+        if(requestCode==REQUEST_CODE_CHEAT){
+            if(date==null){
+                return;
+            }
+        }
+        mIsCheat=CheatActivity.wasAnswerShow(date);
+    }
+
     @Override
     public void onStart(){
         super.onStart();
@@ -161,16 +190,22 @@ public class MainActivity extends AppCompatActivity {
 
     private void checkAnswer(boolean userPressedTrue){
         boolean answerIsTrue=mQuestionBank[mCurrentIndex].isAnswerTrue();
-        int massageResId=0;
-        if(userPressedTrue==answerIsTrue){
-            mQuestionBank[mCurrentIndex].setIsAnswered(1);
-            massageResId=R.string.correct_toast;
-            correctAnswer++;
+        int messageResId=0;
+        if(mIsCheat){
+            messageResId=R.string.judgment_toast;
+        }else {
+            if (userPressedTrue == answerIsTrue) {
+                mQuestionBank[mCurrentIndex].setIsAnswered(1);
+                messageResId = R.string.correct_toast;
+                correctAnswer++;
+            } else {
+                mQuestionBank[mCurrentIndex].setIsAnswered(-1);
+                messageResId = R.string.incorrect_toast;
+            }
         }
-        else {mQuestionBank[mCurrentIndex].setIsAnswered(-1);
-            massageResId=R.string.incorrect_toact;}
             ButtonEnable();
-        Toast.makeText(this,massageResId,Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+
     }
 
 }
